103db121cb304c0316a081e5b6d1fc086d522657
Andreas Grünwald <andreas@grynwald.net>
2024-07-24 17:33:22 +0200

    feat: Add alias "GitLabGetProjectFromRemoteUrl"
    Alias allows extracting the associated GitLab server and project path from a git repository's remote url

---
+++ src/Cake.GitLab/Internal/GitUrl.cs
@@ -0,0 +1,39 @@
+﻿// Derived from https://github.com/ap0llo/changelog/blob/97d0d3111510dae9f4222704a50d88938be8debe/src/ChangeLog/Integrations/GitUrl.cs
+// Licensed under the MIT License
+
+using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Cake.GitLab.Internal;
+
+internal static class GitUrl
+{
+    public static bool TryGetUri(string remoteUrl, [NotNullWhen(true)] out Uri? uri)
+    {
+        if (Uri.TryCreate(remoteUrl, UriKind.Absolute, out uri))
+        {
+            return true;
+        }
+        else
+        {
+            return TryParseScpUrl(remoteUrl, out uri);
+        }
+    }
+
+    private static bool TryParseScpUrl(string url, [NotNullWhen(true)] out Uri? sshUri)
+    {
+        // Parse a scp-format git url: e.g. git@github.com:ap0llo/Cake.GitLab.git
+
+        var fragments = url.Split(':', StringSplitOptions.RemoveEmptyEntries);
+        if (fragments.Length != 2)
+        {
+            sshUri = default;
+            return false;
+        }
+
+        var userNameAndHost = fragments[0];
+        var path = fragments[1].TrimStart('/');
+
+        return Uri.TryCreate($"ssh://{userNameAndHost}/{path}", UriKind.Absolute, out sshUri);
+    }
+}
---
+++ src/Cake.GitLab/Model/GitLabProjectInfo.cs
@@ -0,0 +1,81 @@
+﻿// Derived from https://github.com/ap0llo/changelog/blob/97d0d3111510dae9f4222704a50d88938be8debe/src/ChangeLog/Integrations/GitLab/GitLabProjectInfo.cs
+// Licensed under the MIT License
+
+using System;
+
+namespace Cake.GitLab.Model;
+
+/// <summary>
+/// Encapsualtes the identity of a project on a GitLab server
+/// </summary>
+public sealed class GitLabProjectInfo : IEquatable<GitLabProjectInfo>
+{
+    /// <summary>
+    /// The host name of the GitLab server.
+    /// </summary>
+    public string Host { get; }
+
+    /// <summary>
+    /// The project namespace (the user or group (incl.subgroups) the project belongs to.
+    /// </summary>
+    public string Namespace { get; }
+
+    /// <summary>
+    /// The project name.
+    /// </summary>
+    public string Project { get; }
+
+    /// <summary>
+    /// The GitLab project path (i.e. namespace + repository name).
+    /// </summary>
+    public string ProjectPath => $"{Namespace}/{Project}";
+
+
+    /// <summary>
+    /// Initializes a new instance of <see cref="GitLabProjectInfo"/>
+    /// </summary>
+    /// <param name="host">The host name of the GitLab server.</param>
+    /// <param name="namespace">The GitLab project's namespace (user name or group and subgroup)</param>
+    /// <param name="project">The GitLab project's name</param>
+    /// <exception cref="ArgumentException">Thrown when <paramref name="host"/>, <paramref name="namespace"/> or <paramref name="project"/> is null or whitespace</exception>
+    public GitLabProjectInfo(string host, string @namespace, string project)
+    {
+        if (String.IsNullOrWhiteSpace(host))
+            throw new ArgumentException("Value must not be null or whitespace", nameof(host));
+
+        if (String.IsNullOrWhiteSpace(@namespace))
+            throw new ArgumentException("Value must not be null or whitespace", nameof(@namespace));
+
+        if (String.IsNullOrWhiteSpace(project))
+            throw new ArgumentException("Value must not be null or whitespace", nameof(project));
+
+        Host = host;
+        Namespace = @namespace.Trim('/');
+        Project = project.Trim('/');
+    }
+
+
+    /// <inheritdoc />
+    public override bool Equals(object? obj) => Equals(obj as GitLabProjectInfo);
+
+    /// <inheritdoc />
+    public override int GetHashCode()
+    {
+        unchecked
+        {
+            var hash = StringComparer.OrdinalIgnoreCase.GetHashCode(Host) * 397;
+            hash ^= StringComparer.OrdinalIgnoreCase.GetHashCode(Namespace);
+            hash ^= StringComparer.OrdinalIgnoreCase.GetHashCode(Project);
+            return hash;
+        }
+    }
+
+    /// <inheritdoc />
+    public bool Equals(GitLabProjectInfo? other)
+    {
+        return other is not null &&
+            StringComparer.OrdinalIgnoreCase.Equals(Host, other.Host) &&
+            StringComparer.OrdinalIgnoreCase.Equals(Namespace, other.Namespace) &&
+            StringComparer.OrdinalIgnoreCase.Equals(Project, other.Project);
+    }
+}
