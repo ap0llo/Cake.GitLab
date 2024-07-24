37b3c18962b0fb1926e8ec870951f503e79f097f
Andreas Grünwald <andreas@grynwald.net>
2024-07-24 17:53:58 +0200

    Apply updates from project tempalte

---
+++ test/Cake.GitLab.Test/PublicApiTest.cs
@@ -0,0 +1,38 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.Versioning;
+using System.Threading.Tasks;
+using PublicApiGenerator;
+using VerifyXunit;
+using Xunit;
+
+namespace Cake.GitLab.Test;
+
+[Trait("Category", "SkipWhenLiveUnitTesting")]
+public class PublicApiTest
+{
+    [Fact]
+    public Task Has_expected_public_api()
+    {
+        // ARRANGE
+        
+        var assembly = typeof(GitLabAliases).Assembly;
+        
+
+        var options = new ApiGeneratorOptions()
+        {
+            ExcludeAttributes = new[]
+            {
+                typeof(TargetFrameworkAttribute).FullName!,
+                typeof(AssemblyMetadataAttribute).FullName!,
+                typeof(InternalsVisibleToAttribute).FullName!
+            }
+        };
+
+        // ACT
+        var publicApi = ApiGenerator.GeneratePublicApi(assembly, options);
+
+        // ASSERT        
+        return Verifier.Verify(publicApi, extension: "txt").UseFileName("PublicApi");
+    }
+}
