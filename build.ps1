07b79ffcb412fe31d740d2c3f22716a6db6815e2
Andreas Grünwald <andreas@grynwald.net>
2024-11-17 15:26:29 +0100

    feat: Upgrade to Cake 5.0
    Update the minimum version for this Cake addin to Cake 5.0 and follow the Cake recommendation to target .net8.0 and .net 9.0.
    This removes support for net6.0 and net7.0.
    
    See-Also: https://cakebuild.net/blog/2024/11/cake-v5.0.0-released

--- build.ps1
+++ build.ps1
@@ -4,8 +4,6 @@ if ($env:TF_BUILD) {
     Write-Host "##[group]Install .NET SDK"
 }
 
-./build/dotnet-install.ps1 -Channel 6.0 -Runtime dotnet
-./build/dotnet-install.ps1 -Channel 7.0 -Runtime dotnet
 ./build/dotnet-install.ps1 -Channel 8.0 -Runtime dotnet
 
 # Install SDK and runtime as specified in global.json
