09b929e2239dcccc5428a295f4ff67899ac4d836
Andreas Grünwald <andreas@grynwald.net>
2024-12-26 17:43:12 +0100

    Merge internal PipelinesClient and RepositoryClient classe into DefaultGitLabProvider

--- src/Cake.GitLab/_Provider/DefaultGitLabProvider.cs
+++ src/Cake.GitLab/_Provider/DefaultGitLabProvider.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Net;
 using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using Cake.Common.Build;
@@ -23,6 +25,7 @@ public class DefaultGitLabProvider : IGitLabProvider
     /// Determines whether the current build is running in GitLab CI
     /// </summary>
     private bool IsRunningOnGitLabCI => m_Context.GitLabCI().IsRunningOnGitLabCI;
+
 
     /// <summary>
     /// Initializes the default implementation of <see cref="IGitLabProvider"/> from a build context
@@ -57,7 +60,6 @@ public class DefaultGitLabProvider : IGitLabProvider
 
         return new GitLabServerIdentity(host);
     }
-
 
     public GitLabProjectIdentity? TryGetCurrentProjectIdentity()
     {
@@ -98,17 +100,39 @@ public class DefaultGitLabProvider : IGitLabProvider
         return null;
     }
 
-
     public async Task<Pipeline> GetPipelineAsync(string serverUrl, string accessToken, ProjectId project, int pipelineId)
     {
-        var pipelinesClient = GetPipelinesClient(m_Context, serverUrl, accessToken);
-        return await pipelinesClient.GetPipelineAsync(project, pipelineId);
+        var log = GetLogForCurrentAlias();
+        log.Verbose($"Getting pipeline {pipelineId} from GitLab project {project}");
+
+        var client = GetClient(m_Context, serverUrl, accessToken);
+
+        try
+        {
+            var pipeline = await client.GetPipelines(project).GetByIdAsync(pipelineId);
+            return pipeline;
+        }
+        catch (GitLabException ex)
+        {
+            throw new CakeException($"Error while getting pipeline {pipelineId} from GitLab project {project}: {ex.Message}", ex);
+        }
     }
 
     public async Task SetPipelineNameAsync(string serverUrl, string accessToken, ProjectId project, int pipelineId, string name)
     {
-        var pipelinesClient = GetPipelinesClient(m_Context, serverUrl, accessToken);
-        await pipelinesClient.SetPipelineNameAsync(project, pipelineId, name);
+        var log = GetLogForCurrentAlias();
+        log.Verbose($"Setting name of pipeline {pipelineId} in GitLab project {project} to {name}");
+
+        var client = GetClient(m_Context, serverUrl, accessToken);
+        var pipelinesClient = client.GetPipelines(project);
+        try
+        {
+            await pipelinesClient.UpdateMetadataAsync(pipelineId, new PipelineMetadataUpdate() { Name = name });
+        }
+        catch (GitLabException ex)
+        {
+            throw new CakeException($"Error while setting name of pipeline {pipelineId} in GitLab project {project}: {ex.Message}", ex);
+        }
     }
 
     public async Task RepositoryDownloadFileAsync(string serverUrl, string accessToken, ProjectId project, string filePath, string @ref, FilePath destination)
@@ -117,14 +141,60 @@ public class DefaultGitLabProvider : IGitLabProvider
         Guard.NotNullOrWhitespace(@ref);
         Guard.NotNull(destination);
 
-        var repositoryClient = GetRepositoryClient(m_Context, serverUrl, accessToken);
-        await repositoryClient.DownloadFileAsync(project, filePath, @ref, destination);
+        var log = GetLogForCurrentAlias();
+        log.Verbose($"Downloading {filePath} from GitLab project {project} at reference '{@ref}' to '{destination}'");
+
+        var client = GetClient(m_Context, serverUrl, accessToken);
+        var repo = client.GetRepository(project);
+        FileData fileData;
+        try
+        {
+            log.Debug("Getting file from GitLab");
+            fileData = await repo.Files.GetAsync(filePath, @ref);
+            log.Debug("Received response from GitLab");
+        }
+        catch (GitLabException ex)
+        {
+            throw new CakeException($"Failed to download {filePath} at ref {@ref} from GitLab project {project}: {ex.ErrorMessage ?? ex.Message}", ex);
+        }
+
+        // There is no case when GitLab returns content in an encoding other than base64 (or at least not documented)
+        // To prevent wrong data from being written in case this changes in the future, abort if encoding is unexpected
+        if (!StringComparer.Ordinal.Equals(fileData.Encoding, "base64"))
+        {
+            throw new InvalidOperationException($"Unexpected encoding of file content recevied from GitLab. Expected: base64, Actual: {fileData.Encoding}");
+        }
+
+        var destinationDirectory = destination.GetDirectory();
+        log.Debug($"Creating destination directory '{destination}'");
+        m_Context.FileSystem.GetDirectory(destinationDirectory).Create();
+
+        log.Debug($"Writing contents to destination file '{destination}'");
+        using var outStream = m_Context.FileSystem.GetFile(destination).OpenWrite();
+        await outStream.WriteAsync(Convert.FromBase64String(fileData.Content));
+
+        log.Verbose("File downloaded successfully");
     }
 
-    public async Task<IReadOnlyCollection<Branch>> RepositoryGetBranchesAsync(string serverUrl, string accessToken, ProjectId project)
+    public Task<IReadOnlyCollection<Branch>> RepositoryGetBranchesAsync(string serverUrl, string accessToken, ProjectId project)
     {
-        var repositoryClient = GetRepositoryClient(m_Context, serverUrl, accessToken);
-        return await repositoryClient.GetBranchesAsync(project);
+        var log = GetLogForCurrentAlias();
+        log.Verbose($"Getting branches from GitLab project {project}");
+
+        var client = GetClient(m_Context, serverUrl, accessToken);
+        var repo = client.GetRepository(project);
+        Branch[] branches;
+        try
+        {
+            branches = repo.Branches.All.ToArray();
+        }
+        catch (GitLabException ex)
+        {
+            throw new CakeException($"Failed to get branches from GitLab project {project}: {ex.ErrorMessage ?? ex.Message}", ex);
+        }
+
+        log.Verbose($"Loaded data for {branches.Length} branches");
+        return Task.FromResult<IReadOnlyCollection<Branch>>(branches);
     }
 
     public async Task<Tag> RepositoryCreateTagAsync(string serverUrl, string accessToken, ProjectId project, string @ref, string name)
@@ -132,21 +202,45 @@ public class DefaultGitLabProvider : IGitLabProvider
         Guard.NotNullOrWhitespace(@ref);
         Guard.NotNullOrWhitespace(name);
 
-        var repositoryClient = GetRepositoryClient(m_Context, serverUrl, accessToken);
-        return await repositoryClient.CreateTagAsync(project, @ref, name);
+
+        var log = GetLogForCurrentAlias();
+        log.Verbose($"Creating tag '{name}' for reference '{@ref}' in GitLab project {project}");
+
+        var client = GetClient(m_Context, serverUrl, accessToken);
+
+        // 'ref' might be a branch or tag name or abbreviated commit SHA
+        // To make comparisons easier, get the commit for the reference (and thus let the GitLab server perform the matching of the ref to a commit)
+        var commit = GetCommitInternal(log, client, project, @ref);
+
+        // Check if ´the tag already exists with the same target id
+        var existingTag = await TryGetTagInternalAsync(log, client, project, name);
+        if (existingTag?.Commit?.Id == commit.Id)
+        {
+            log.Verbose($"Tag '{existingTag.Name}' for commit {existingTag.Commit.Id} already exists");
+            return existingTag;
+        }
+
+        Tag tag;
+        try
+        {
+            // Create a new tag using the commit id as reference
+            // By using the commit id, we can ensure there is no race-condition between the check if a tag already exists and the creation of the tag.
+            // Otherwise, when 'ref' is e.g. a branch name, the commit the ref refers to might change between getting existing tags and creation of new tags
+            var tagCreate = new TagCreate() { Ref = commit.Id.ToString(), Name = name };
+            log.Debug($"Creating tag '{tagCreate.Name}' for reference '{tagCreate.Ref}'");
+            tag = client.GetRepository(project).Tags.Create(tagCreate);
+        }
+        catch (GitLabException ex)
+        {
+            throw new CakeException($"Failed to create tag '{name}' for reference '{@ref}' in GitLab project {project}: {ex.ErrorMessage ?? ex.Message}", ex);
+        }
+
+        log.Verbose($"Tag '{tag.Name}' for commit {tag.Commit.Id} created successfully");
+        return tag;
     }
 
     private DebugLog GetLogForCurrentAlias([CallerMemberName] string methodName = "") => new DebugLog(m_Context.Log, $"GitLab.{methodName}");
 
-
-    //TODO: Merge PipelinesClient and Provider classey
-    private static PipelinesClient GetPipelinesClient(ICakeContext context, string serverUrl, string accessToken, [CallerMemberName] string aliasName = "")
-    {
-        var gitLabClient = GetClient(context, serverUrl, accessToken);
-        var log = GetLogForCurrentAlias(context, aliasName);
-        var pipelinesClient = new PipelinesClient(log, context.FileSystem, gitLabClient);
-        return pipelinesClient;
-    }
 
     private static IGitLabClient GetClient(ICakeContext context, string serverUrl, string accessToken, [CallerMemberName] string aliasName = "")
     {
@@ -175,12 +269,43 @@ public class DefaultGitLabProvider : IGitLabProvider
     private static DebugLog GetLogForCurrentAlias(ICakeContext context, [CallerMemberName] string aliasName = "") => new DebugLog(context.Log, aliasName);
 
 
-    private static RepositoryClient GetRepositoryClient(ICakeContext context, string serverUrl, string accessToken, [CallerMemberName] string aliasName = "")
+
+    private Commit GetCommitInternal(ICakeLog log, IGitLabClient client, ProjectId project, string @ref)
     {
-        var gitLabClient = GetClient(context, serverUrl, accessToken);
-        var log = GetLogForCurrentAlias(context, aliasName);
-        var repositoryClient = new RepositoryClient(log, context.FileSystem, gitLabClient);
-        return repositoryClient;
+        Commit commit;
+        try
+        {
+            log.Debug($"Getting commit for reference '{@ref}'");
+            commit = client.GetCommits(project).GetCommit(@ref);
+            log.Debug($"Commit for reference '{@ref}' is {commit.Id}");
+        }
+        catch (GitLabException ex)
+        {
+            throw new CakeException($"Failed to get commit for reference '{@ref}' from GitLab project {project}: {ex.ErrorMessage ?? ex.Message}", ex);
+        }
+
+        return commit;
     }
 
+    private async Task<Tag?> TryGetTagInternalAsync(ICakeLog log, IGitLabClient client, ProjectId project, string name)
+    {
+        Tag tag;
+        try
+        {
+            log.Debug($"Attempting to get tag '{name}'");
+            tag = await client.GetRepository(project).Tags.GetByNameAsync(name);
+            log.Debug($"Found tag '{tag.Name}' for commit '{tag.Commit.Id}'");
+        }
+        catch (GitLabException ex) when (ex.StatusCode == HttpStatusCode.NotFound)
+        {
+            log.Debug($"Tag '{name}' does not exist. Getting tag failed with status code '{ex.StatusCode}'");
+            return null;
+        }
+        catch (GitLabException ex)
+        {
+            throw new CakeException($"Failed to get tag '{name}' from GitLab project {project}: {ex.ErrorMessage ?? ex.Message}", ex);
+        }
+
+        return tag;
+    }
 }
